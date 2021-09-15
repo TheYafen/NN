c4f1ebbc0d44bdbcfed3b628fa8468b1c89176f2
theyafen <theyafen@gmail.com>
2021-09-15 16:26:52 +0300

    n

--- main.py
+++ main.py
@@ -11,23 +11,32 @@ def mse(y_true, y_pred):
     return ((y_true-y_pred)**2).mean()
 
 # Weights
-w1 = np.random.normal()
-w2 = np.random.normal()
-w3 = np.random.normal()
-w4 = np.random.normal()
-w5 = np.random.normal()
-w6 = np.random.normal()
+w = np.random.normal(6)
 
 # Biases
-b1 = np.random.normal()
-b2 = np.random.normal()
-b3 = np.random.normal()
+b = np.random.normal(3)
 
-
-def n(inputs, bias):
-    sum = inputs[0]*weights[0] + inputs[1]*weights[1] + bias
+def n(w):
+    sum = np.dot(x, w) + b
     output = sigmoid(sum)
     return output
 
-def feedforward():
-    pass
+# Train
+
+def train():
+
+    # Feed forward
+    h1 = sigmiod(x[0]*w[0] + x[1]*w[1] + b[0])
+    h2 = sigmiod(x[0]*w[2] + x[1]*w[3] + b[2])
+    o1 = sigmiod(h1  *w[4] + h2  *w[5] + b[3])
+    y_pred = o1
+
+    # Back propogation
+    ## d_L_d_y_pred
+    d_L_d_ypred = -2 * (y_true - y_pred)
+
+    ##Neuron o1
+    d_ypred_d_w4 = h1 * d_sigmoid(o1)
+    d_ypred_d_w5 = h1 * d_sigmoid(o1)
+
+    d_ypred_d_b3
